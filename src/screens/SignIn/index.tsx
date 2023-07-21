13bf8e1c3d94600135b8221d35f745b29ab5bdea
Juan Carlos <juan2017amaral@gmail.com>
2023-07-20 20:42:31 -0400

    fix: Login with email

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import { Alert } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
+import { Realm, useApp } from "@realm/react";
 
 import { Container, Title, Slogan } from "./styles";
 
@@ -9,8 +11,6 @@ import backgroundImg from "../../assets/background.png";
 import { Button } from "../../components/Button";
 
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
-
-import { Realm, useApp } from "@realm/react";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -40,8 +40,14 @@ export function SignIn() {
         const credentials = Realm.Credentials.jwt(
           response.authentication.idToken
         );
+
         app.logIn(credentials).catch((error) => {
           console.log(error);
+          Alert.alert(
+            "Entrar",
+            "Não foi possível conectar-se a sua conta google."
+          );
+          setIsAuthenticating(false);
         });
       } else {
         Alert.alert(
